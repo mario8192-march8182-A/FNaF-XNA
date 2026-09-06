1710cca06433db60e43fa6efe4fa4a940ffeecf7
Altgr109 <joseotaviohinckel@gmail.com>
2026-09-06 14:15:03 -0300

    Add AssemblyInfo.cs - Assembly metadata and version information

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FNaF-XNA")]
+[assembly: AssemblyProduct("Five Nights at Freddy's - XNA Framework")]
+[assembly: AssemblyDescription("A Five Nights at Freddy's game implementation using XNA Framework")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9E87E76D-3C96-49B0-9C63-42D1F7A6E1C2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
